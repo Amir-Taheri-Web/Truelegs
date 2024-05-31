028eb8ea608de660d2aa477bfdefc3360fc4e0ae
Amir Taheri <amir.taheri.web@protonmail.com>
2024-05-31 10:59:59 +0330

    chore: layout created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,19 @@
-import ThemeSwitcher from "@/modules/ThemeSwitch";
-
 const Home = () => {
   return (
-    <div>
-      <ThemeSwitcher />
+    <div className="max-container">
+      <header>Header</header>
+
+      <main>
+        <section className="padding-y">Hero</section>
+        <section className="padding-y">Popular Products</section>
+        <section className="padding-y">Super Quality</section>
+        <section className="sm:py-12 py-4">Services</section>
+        <section className="padding-y">Special</section>
+        <section className="padding-y">Customers</section>
+        <section className="padding-y">Updates</section>
+      </main>
+
+      <footer>Footer</footer>
     </div>
   );
 };
