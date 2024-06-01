b75b6a15476db4aa27d4e8a99d5c87f070901232
Amir Taheri <amir.taheri.web@protonmail.com>
2024-06-01 11:11:20 +0330

    refactor: services updated

---
+++ src/components/elements/ServicesCard.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const ServicesCard = ({ item }) => {
+  return (
+    <li className="shadow-3xl rounded-2xl px-10 py-14 flex flex-col gap-4 flex-1">
+      <div className="bg-coral-red p-2 w-[45px] h-[45px] rounded-full flex justify-center items-center">
+        <Image src={item.icon} alt={item.title} width={25} height={25} />
+      </div>
+
+      <h3 className="text-3xl font-semibold">{item.title}</h3>
+
+      <p className="text-slate-gray leading-6">{item.description}</p>
+    </li>
+  );
+};
+
+export default ServicesCard;
